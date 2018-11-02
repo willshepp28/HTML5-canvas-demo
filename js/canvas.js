be4d8d530f1ff0eb172a716f32ebda8620cec063
willshepp28 <willsheppard28@outlook.com>
2018-11-02 11:21:12 -0400

    make a setcoordinates function to set the coordinates of any lines we draw. And a circle function to create a circle.

--- js/canvas.js
+++ js/canvas.js
@@ -6,52 +6,48 @@ window.onload = function () {
     var canvas = document.querySelector("canvas");
     var ctx = canvas.getContext("2d");
 
-    bottomOfGallow(ctx)
-    middleOfGallow(ctx);
-    topGallow(ctx)
-    gallowTopDrop(ctx);
-    hangmanHead(ctx)
+/*
+|--------------------------------------------------------------------------
+| Sets the coordinates of the gallows
+|--------------------------------------------------------------------------
+*/
+    var bottomOfGallow = setcoordinates(90,275,250,275);
+    var middleOfGallow = setcoordinates(175,275,175,60);
+    var topGallow = setcoordinates(110,60,175,60);
+    var gallowDrop = setcoordinates(110,60,110,108);
 
 
 
-    function bottomOfGallow(ctx) {
+/*
+|--------------------------------------------------------------------------
+| Sets the coordinates of the hangman
+|--------------------------------------------------------------------------
+*/
+    var head = circle(110,123,15);
+    var torso = setcoordinates(110,137,110,188);
+
+
+
+    // creates a circle
+    function circle(xAxis,yAxis, radius ) {
         ctx.beginPath();
-        ctx.moveTo(90, 275);
-        ctx.lineTo(250, 275);
+        ctx.arc(xAxis, yAxis, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+
+
+
+    // Sets the coordinates of lines
+    function setcoordinates(moveStart, moveEnd, lineStart,lineEnd) {
+        ctx.beginPath();
+        ctx.moveTo(moveStart, moveEnd);
+        ctx.lineTo(lineStart, lineEnd);
         ctx.strokeStyle = "black";
         ctx.stroke();
     }
+   
 
-
-    function middleOfGallow(ctx) {
-        ctx.beginPath();
-        ctx.moveTo(175, 275);
-        ctx.lineTo(175, 60);
-        ctx.stroke();
-    }
-
-
-    function topGallow(ctx) {
-        ctx.beginPath();
-        ctx.moveTo(110, 60);
-        ctx.lineTo(175, 60);
-        ctx.strokeStyle = "black";
-        ctx.stroke();
-
-    }
-
-    function gallowTopDrop(ctx) {
-        ctx.beginPath();
-        ctx.moveTo(110, 60);
-        ctx.lineTo(110, 110);
-        ctx.strokeStyle = "black";
-        ctx.stroke();
-    }
-
-
-    function hangmanHead(ctx) {
-        ctx.beginPath();
-        ctx.arc(110, 123, 15, 0, 2 * Math.PI);
-        ctx.stroke();
-    }
 }
+
+
